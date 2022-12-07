6f940500e90edc6de4733aaa2becc60aafd5091a
Harika Ramidi <ramidi.h@northeastern.edu>
2022-12-07 17:33:20 -0500

    urls placed in config

--- server/src/config/env-config.ts
+++ server/src/config/env-config.ts
@@ -1,4 +1,5 @@
 export default {
     GoogleClientId: "22014990618-hd5bkqr2r4mida0ou8s7ginjbtikjnok.apps.googleusercontent.com",
-    GoodleClientSecret: "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"
+    GoodleClientSecret: "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",
+    GoogleCallBackURL: "http://localhost:8080/api/v1/auth/google/callback"
    }
